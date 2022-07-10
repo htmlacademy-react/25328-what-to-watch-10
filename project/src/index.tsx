a8d637082043d2d1d8594103b9013cd82385e790
Saint-Hedgehog <svyatoiej@gmail.com>
2022-07-10 15:15:13 +0300

    module2-task1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  NAME: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  YEAR: 2014,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App name={Setting.NAME} genre={Setting.GENRE} year={Setting.YEAR} />
   </React.StrictMode>,
 );
